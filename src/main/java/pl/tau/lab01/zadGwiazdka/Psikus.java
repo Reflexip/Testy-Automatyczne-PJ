c1edfc3f042fcf3241592197732d0e5462a35fbe
Dawid Mudlaff <dawid.mudlaff@gmail.com>
2016-10-23 12:31:05 +0200

    Delete Psikus.java

--- src/main/java/pl/tau/lab01/zadGwiazdka/Psikus.java
+++
@@ -1,7 +0,0 @@
-package pl.tau.lab01.zadGwiazdka;
-
-public interface Psikus {
-    Integer cyfrokrad(Integer liczba);
-    Integer hultajchochla(Integer liczba) throws NieudanyPsikusException;
-    Integer niekształtek(Integer liczba);
-}
